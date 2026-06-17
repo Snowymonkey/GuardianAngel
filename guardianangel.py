d902eddbdb7e224bbe4690fc923092b2952ff261
Snowymonkey <143772406+Snowymonkey@users.noreply.github.com>
2026-06-17 16:59:33 -0500

    Added SSH spam detection.

--- guardianangel.py
+++ guardianangel.py
@@ -7,6 +7,7 @@ import json
 with open("config.json", "r") as json_file:
     data = json.load(json_file)
     syn_threshold = data["syn_threshold"]
+    ssh_threshold = data["ssh_threshold"]
 
 block_ip = "sudo iptables -I INPUT 1 -s 1.1.1.1 -j DROP"
 one_liner = "sudo iptables -I INPUT 1 -s 1.1.1.1 -j DROP && sudo iptables -L INPUT -n"
@@ -21,14 +22,19 @@ ip_tracker = {
 
 def create_ip_tracker():
     return {
-        "syn" : {"time" : None, "ports" : {}},
-        "ssh" : {"time" : None, "attempts" : None},
+        "syn" : {"time" : None, "ports" : set()},
+        "ssh" : {"time" : None, "attempts" : 0},
         "status" : "OPEN"
     }
 
-# def check_blocked_and_new_ips(ip):
+def block(ip):
+    print(f"[*] Configuring firewall to BLOCK {ip}...")
+    ip_tracker[ip]["status"] = "BLOCKED"
+    # subprocess.run(["sudo", "iptables", "-I", "FORWARD", "1", "-s", ip, "-j", "DROP"])
+    print("[*] Guardian Angel has BLOCKED", ip)
 
 def analyze(packet):
+    
     if not (packet.haslayer(IP) and packet.haslayer(TCP)):
         return
     
@@ -42,27 +48,25 @@ def analyze(packet):
         
         if source_ip not in ip_tracker:
             ip_tracker[source_ip] = create_ip_tracker()
-            tracked_ip = ip_tracker[source_ip]["syn"]
-            tracked_ip["time"] = timestamp
-        
+            ip_tracker[source_ip]["syn"]["time"] = timestamp
+            
+        if ip_tracker[source_ip]["syn"]["time"] is None:
+            ip_tracker[source_ip]["syn"]["time"] = timestamp
+            
         time_passed = timestamp - ip_tracker[source_ip]["syn"]["time"]
 
         if time_passed > 1:
-            tracked_ip["time"] = timestamp
-            tracked_ip["ports"] = {dport}
+            ip_tracker[source_ip]["syn"]["time"] = timestamp
+            ip_tracker[source_ip]["syn"]["ports"] = {dport}
         else:
-            tracked_ip["ports"].add(dport)
+            ip_tracker[source_ip]["syn"]["ports"].add(dport)
         
-        if len(tracked_ip["ports"]) > syn_threshold:
+        if len(ip_tracker[source_ip]["syn"]["ports"]) > syn_threshold:
             print("[!] SYN port scan from", source_ip)
-            tracked_ip["status"] = "BLOCKED"
-            print(f"[*] Configuring firewall to BLOCK {source_ip}...")
-            subprocess.run(["sudo", "iptables", "-I", "FORWARD", "1", "-s", source_ip, "-j", "DROP"])
-            print("[*] Guardian Angel has BLOCKED", source_ip)
+            block(source_ip)
 
 
     elif packet[TCP].flags == 0: ## Null Scan Detection
-
         source_ip = packet[IP].src
         timestamp = packet.time
         dport = packet[TCP].dport
@@ -72,13 +76,9 @@ def analyze(packet):
         
         if source_ip not in ip_tracker:
             ip_tracker[source_ip] = create_ip_tracker()
-        else:
-            ip_tracker[source_ip]["status"] = "BLOCKED"
         
         print("[!] Null port scan from", source_ip)
-        print(f"[*] Configuring firewall to BLOCK {source_ip}...")
-        subprocess.run(["sudo", "iptables", "-I", "FORWARD", "1", "-s", source_ip, "-j", "DROP"])
-        print("[*] Guardian Angel has BLOCKED", source_ip)
+        block(source_ip)
 
     elif packet[TCP].flags == "FPU": ## Xmas Scan Detection
 
@@ -91,17 +91,37 @@ def analyze(packet):
         
         if source_ip not in ip_tracker:
             ip_tracker[source_ip] = create_ip_tracker()
-        else:
-            ip_tracker[source_ip]["status"] = "BLOCKED"
         
         print("[!] Xmas port scan from", source_ip)
-        print(f"[*] Configuring firewall to BLOCK {source_ip}...")
-        subprocess.run(["sudo", "iptables", "-I", "FORWARD", "1", "-s", source_ip, "-j", "DROP"])
-        print(f"[*] Guardian Angel has BLOCKED {source_ip}") 
+        block(source_ip)
     
     elif packet[TCP] and packet.dport == 22: ## SSH Scan detection
-        print("SSH Packet")
 
+        source_ip = packet[IP].src
+        timestamp = packet.time
+        dport = packet[TCP].dport
+
+        if source_ip in ip_tracker and ip_tracker[source_ip]["status"] == "BLOCKED":
+            return
+        
+        if source_ip not in ip_tracker:
+            ip_tracker[source_ip] = create_ip_tracker()
+            ip_tracker[source_ip]["ssh"]["time"] = timestamp
+        
+        if ip_tracker[source_ip]["ssh"]["time"] is None:
+            ip_tracker[source_ip]["ssh"]["time"] = timestamp
+        
+        time_passed = timestamp - ip_tracker[source_ip]["ssh"]["time"]
+
+        if time_passed > 1:
+            ip_tracker[source_ip]["ssh"]["time"] = timestamp
+            ip_tracker[source_ip]["ssh"]["attempts"] = 0
+        else:
+            ip_tracker[source_ip]["ssh"]["attempts"] += 1
+        
+        if ip_tracker[source_ip]["ssh"]["attempts"] > ssh_threshold:
+            print("[!] SSH brute force from", source_ip)
+            block(source_ip)
 
 
 if __name__ == "__main__":
